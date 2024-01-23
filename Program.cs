67caa48735accc78f54adc0257b8c3e02ed1e572
Mason <3zP3UvPxns6PJ27j@proton.me>
2024-01-23 13:18:06 -0500

    Added Project Files

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Game
+{
+    internal class Program
+    {
+
+        public class Player
+        {
+            static int x = 3;
+            static int y = 7;
+        }
+
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < roomHeight; i++)
+            {
+                for (int j = 0; j < roomWidth; j++)
+                {
+                    if (i == 0 || i == roomHeight - 1)
+                    {
+                        Console.Write("#");
+                    }
+                    else if(j == 0 || j == roomWidth - 1)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
