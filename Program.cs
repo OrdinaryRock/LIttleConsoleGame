1e0f831b0501a690283b09bdd4ca61a348731fdb
Mason <3zP3UvPxns6PJ27j@proton.me>
2024-01-24 10:38:18 -0500

    And a moveable player entity

--- Program.cs
+++ Program.cs
@@ -6,36 +6,95 @@ namespace Game
 {
     internal class Program
     {
-
+        static int mapWidth = 32;
+        static int mapHeight = 12;
+        static string[][] map = new string[mapHeight][];
         public class Player
         {
-            static int x = 3;
-            static int y = 7;
+            public static int x = 3;
+            public static int y = 7;
         }
 
         static void Main(string[] args)
         {
-            for(int i = 0; i < roomHeight; i++)
+            InitMap();
+            DoTurn();
+
+            
+
+            
+
+            
+        }
+        static void InitMap()
+        {
+            for (int i = 0; i < mapHeight; i++)
             {
-                for (int j = 0; j < roomWidth; j++)
+                map[i] = new string[mapWidth];
+            }
+            for (int i = 0; i < mapHeight; i++)
+            {
+                for (int j = 0; j < mapWidth; j++)
                 {
-                    if (i == 0 || i == roomHeight - 1)
-                    {
-                        Console.Write("#");
-                    }
-                    else if(j == 0 || j == roomWidth - 1)
-                    {
-                        Console.Write("#");
-                    }
-                    else
-                    {
-                        Console.Write(" ");
-                    }
+                    if (i == 0 || i == mapHeight - 1) map[i][j] = "#";
+                    else if (j == 0 || j == mapWidth - 1) map[i][j] = "#";
+                    else map[i][j] = " ";
+                }
+            }
+        }
+        static void DrawMap()
+        {
+            Console.Clear();
+            for (int i = 0; i < mapHeight; i++)
+            {
+                for (int j = 0; j < mapWidth; j++)
+                {
+                    string tile = map[i][j];
+                    if (Player.x == j && Player.y == i) Console.Write("@");
+                    else Console.Write(tile);
                 }
                 Console.WriteLine();
             }
 
-            Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Please enter your desired action!");
+            Console.WriteLine("Up - Move one space up");
+            Console.WriteLine("Down - Move one space down");
+            Console.WriteLine("Left - Move one space left");
+            Console.WriteLine("Right - Move one space right");
+            Console.Write("\n>");
+        }
+
+        static void DoTurn()
+        {
+            DrawMap();
+            string input = Console.ReadLine().ToLower();
+            switch(input)
+            {
+                case "quit":
+                    QuitGame();
+                    return;
+                    break;
+                case "up":
+                    Player.y--;
+                    break;
+                case "down":
+                    Player.y++;
+                    break;
+                case "left":
+                    Player.x--;
+                    break;
+                case "right":
+                    Player.x++;
+                    break;
+            }
+            DoTurn();
+        }
+
+        static void QuitGame()
+        {
+            Console.Clear();
+            Console.WriteLine("Thank you for playing!");
         }
     }
 }
--- obj/Debug/net8.0/Game.AssemblyInfo.cs
+++ obj/Debug/net8.0/Game.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67caa48735accc78f54adc0257b8c3e02ed1e572")]
 [assembly: System.Reflection.AssemblyProductAttribute("Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
